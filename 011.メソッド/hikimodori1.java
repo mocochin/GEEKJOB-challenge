ab62b28e193baffa3dd70db2b4ac563162cbbfcb
今井翔平 <imaishohei0314@gmail.com>
2018-05-25 10:25:18 +0900

    引数、戻り数　完了しました。

---
+++ 011.メソッド/hikimodori1.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author PC1
+ */
+public class hikimodori1 extends HttpServlet {
+
+    String[] data1 = {"0001", "1997/03/14", "長野県木曽"};
+    String[] data2 = {"0002", "1996/04/02", "東京都世田谷区"};
+    String[] data3 = {"0003", "1999/11/24", "長野県松本市"};
+    
+
+    
+    String[] hello(String name) {
+    
+     if (name == "0001"){        
+        return data1;
+        }
+        
+     else if (name == "0002"){        
+        return data2;
+     }
+     
+     else if (name == "0003"){         
+         return data3;
+     }
+     
+     else return null;
+     
+    
+    }
+    
+    
+    
+    
+   
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet hikimodori1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            
+            
+            String[] newhello = hello("0001");
+            
+            out.println(Arrays.toString(newhello));
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
